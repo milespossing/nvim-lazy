28a494e7a6053723d187ab99663aaf272b27c60d
Miles Possing <mpossing@pop-os.localdomain>
2023-01-10 22:09:19 -0800

    Updating options and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local wk = require("which-key")
+
+wk.register({
+  T = {
+    name = "terminal",
+    n = { "<cmd>split term://zsh<cr>", "New Terminal" },
+  },
+}, { prefix = "<leader>" })
