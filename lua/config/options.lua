dc454abc0a2e9bbf0b90f9b9c77ae7e8f2ca4510
Miles Possing <milespossing@microsoft.com>
2023-10-13 14:06:45 -0500

    Updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,14 @@
 -- Add any additional options here
 vim.g.mapleader = " "
 vim.opt.confirm = false
+
+local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = "wsl clipboard",
+    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true,
+  }
+end
